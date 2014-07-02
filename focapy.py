cdd9a12795b8c7251fdfe569263c9f01a075f124
Albert Huang <alberthrocks@users.noreply.github.com>
2014-07-02 09:36:17 -0400

    Major changes, feature additions, and fixes
     * Fixed 'type' regex where a capturing group was not defined.
     * Added capturing group length check to ensure a capturing group was
       defined. If no capturing groups are found, the program errors.
     * Added %POINTER% variable for defining a place to place the pointer.
     * Fixed consistency issue with pointers for global variables.
     * Added more information about length inconsistencies between regex
       matches and maps.
     * Fixed bug where map_dict was being initialized as a list, not a dict.
     * Fixed bug where replacing variables failed due to non-string
       replacement variables.
     * Removed hacky implementations for Fortran types 'character' and
       'type'.
     * Implemented custom types for derived types and arguments.
     * Fixed ident issue for arguments portion.
     * Added padding for generated C header file.

--- focapy.py
+++ focapy.py
@@ -67,13 +67,13 @@ custom_types = {
                             {
                                 'regex':
                                         [
-                                            r'type\(\w+\)',
+                                            r'type\((\w+)\)',
                                         ],
                                 'map'   :
                                         [
                                             'type_name=%STRING%',
                                         ],
-                                'format':   '%%type_name%% %NAME%',
+                                'format':   '%%type_name%% %POINTER%%NAME%',
                             },
                }
                                         
@@ -113,7 +113,7 @@ for mod, name in modules:
                         if found_match.isdigit():
                             found_match = int(found_match)
                         if found_match in type_table[type_base]:
-                            header_fh.write("%s %s;\n" % (type_table[type_base][found_match], element.name))
+                            header_fh.write("%s *%s;\n" % (type_table[type_base][found_match], element.name))
                             subst_made = True
                             break
                 if not subst_made:
@@ -136,10 +136,13 @@ for mod, name in modules:
                     match = regex.search(type)
                     if match:
                         # Map check
-                        if len(custom_types[type_base]["map"]) != len(match.groups()):
-                            print "ERROR: Number of regex matches does not match number of maps!"
+                        if len(match.groups()) == 0:
+                            print "ERROR: No match groups defined for regex!"
                             sys.exit(1)
-                        map_dict = []
+                        if len(custom_types[type_base]["map"]) != len(match.groups()):
+                            print "ERROR: Number of regex matches does not match number of maps! (Map has %i, match has %i)" % (len(custom_types[type_base]["map"]), len(match.groups()))
+                            sys.exit(1)
+                        map_dict = {}
                         map_res = match.groups()
                         for i in xrange(0, len(map_res)):
                             # Validate
@@ -164,10 +167,10 @@ for mod, name in modules:
                 final_format = custom_types[type_base]["format"]
                 # Variable replacement
                 for var in map_dict.keys():
-                    final_format = final_format.replace("%%" + var + "%%", map_dict[var])
+                    final_format = final_format.replace("%%" + var + "%%", str(map_dict[var]))
                 # Static variable replacement
                 final_format = final_format.replace("%NAME%", element.name);
-                final_format = final_format.replace("%POINTER%", "");
+                final_format = final_format.replace("%POINTER%", "*");
                 
                 # Write it out!
                 header_fh.write(final_format + ";\n")
@@ -175,10 +178,10 @@ for mod, name in modules:
                 print "WARNING: Could not find equivalent C definition for type '%s'!" % type
                 print "Make sure you define it!"
                 header_fh.write("// STUB: %s %s\n" % (type, element.name))
-
+        header_fh.write("\n")
     if len(mod.types) > 0:
         print "  Types:"
-        header_fh.write('#pragma pack(2)\n')
+        header_fh.write('#pragma pack(2)\n\n')
         for type in mod.types:
             print "    Type '%s':" % type.name
             header_fh.write("typedef struct %s_ {\n" % type.name)
@@ -194,11 +197,11 @@ for mod, name in modules:
                     elif type_ele_t_base in type_table.keys():
                         subst_made = False
                         for generic_re in generic_regex:
-                            print "DEBUG: SEARCH: "+(type_ele_t_base + generic_re)
+                            #print "DEBUG: SEARCH: "+(type_ele_t_base + generic_re)
                             regex = re.compile(type_ele_t_base + generic_re)
                             match = regex.search(type_ele_t)
                             if match:
-                                print "MATCH!"
+                                #print "MATCH!"
                                 found_match = match.groups()[0]
                                 if found_match.isdigit():
                                     found_match = int(found_match)
@@ -210,15 +213,69 @@ for mod, name in modules:
                             print "WARNING: Could not find equivalent C definition for type '%s'!" % type_ele_t
                             print "Make sure you define it!"
                             header_fh.write("    // STUB: %s %s\n" % (type_ele_t, type_ele.name))
-                    # Hacky
-                    elif type_ele_t == 'character(len=20)':
-                        header_fh.write("    char %s[20];\n" % (type_ele.name))
+                    elif type_ele_t_base in custom_types.keys():
+                        # Validation
+                        if "regex" not in custom_types[type_ele_t_base]:
+                            print "ERROR: Custom types dict is invalid! (Regex definition not found!)"
+                            sys.exit(1)
+                        if "map" not in custom_types[type_ele_t_base]:
+                            print "ERROR: Custom types dict is invalid! (Map definition not found!)"
+                            sys.exit(1)
+                        if "format" not in custom_types[type_ele_t_base]:
+                            print "ERROR: Custom types dict is invalid! (Map definition not found!)"
+                            sys.exit(1)
+                        for reg in custom_types[type_ele_t_base]["regex"]:
+                            regex = re.compile(reg)
+                            match = regex.search(type_ele_t)
+                            if match:
+                                # Map check
+                                if len(match.groups()) == 0:
+                                    print "ERROR: No match groups defined for regex!"
+                                    sys.exit(1)
+                                if len(custom_types[type_ele_t_base]["map"]) != len(match.groups()):
+                                    print "ERROR: Number of regex matches does not match number of maps! (Map has %i, match has %i)" % (len(custom_types[type_base]["map"]), len(match.groups()))
+                                    sys.exit(1)
+                                map_dict = {}
+                                map_res = match.groups()
+                                for i in xrange(0, len(map_res)):
+                                    # Validate
+                                    map_split = custom_types[type_ele_t_base]["map"][i].split("=")
+                                    if len(map_split) != 2:
+                                        print "ERROR: Map specification is invalid! It should be variable=%TYPE%!"
+                                        sys.exit(1)
+                                    if not ((map_split[1][0] == "%") and (map_split[1][-1] == "%")):
+                                        print "ERROR: Map specification is invalid! The type part is invalid - it should be variable=%TYPE%!"
+                                        sys.exit(1)
+                                    map_name = map_split[0]
+                                    map_type = map_split[1].upper()
+                                    #print map_dict
+                                    if map_type == "%INT%":
+                                        map_dict[map_name] = int(map_res[i])
+                                    elif map_type == "%FLOAT%":
+                                        map_dict[map_name] = float(map_res[i])
+                                    elif map_type == "%STRING%":
+                                        map_dict[map_name] = str(map_res[i])
+                                    else:
+                                        print "ERROR: Map specification is invalid! (Invalid type found - valid: %INT%, %FLOAT%, %STRING%)"
+                                        sys.exit(1)
+                        final_format = custom_types[type_ele_t_base]["format"]
+                        # Variable replacement
+                        for var in map_dict.keys():
+                            final_format = final_format.replace("%%" + var + "%%", str(map_dict[var]))
+                        # Static variable replacement
+                        final_format = final_format.replace("%NAME%", type_ele.name);
+                        final_format = final_format.replace("%POINTER%", "*");
+                        
+                        # Write it out!
+                        header_fh.write("    " + final_format + ";\n")
                     else:
                         print "WARNING: Could not find equivalent C definition for type '%s'!" % type_ele_t
                         print "Make sure you define it!"
                         header_fh.write("    // STUB: %s %s\n" % (type_ele_t, type_ele.name))
                 header_fh.write("} %s, *p_%s;\n" % (type.name, type.name))
-        header_fh.write('#pragma pack()\n')
+                header_fh.write("\n")
+            #header_fh.write("\n")
+        header_fh.write('#pragma pack()\n\n')
     
     if len(mod.subts) > 0:
         print "  Subroutines:"
@@ -258,13 +315,62 @@ for mod, name in modules:
                             print "WARNING: Could not find equivalent C definition for type '%s'!" % type
                             print "Make sure you define it!"
                             header_fh.write("/* STUB: %s %s,*/ " % (type, sub_arg.name))
-                    # Hacky
-                    elif type == "type(header_list)":
-                        args_list.append("header_list *%s" % (sub_arg.name))
-                        args_map[type] = "header_list *%s" % (sub_arg.name)
-                    elif type == "type(data_list)":
-                        args_list.append("data_list *%s" % (sub_arg.name))
-                        args_map[type] = "data_list *%s" % (sub_arg.name)
+                    elif type_base in custom_types.keys():
+                        # Validation
+                        if "regex" not in custom_types[type_base]:
+                            print "ERROR: Custom types dict is invalid! (Regex definition not found!)"
+                            sys.exit(1)
+                        if "map" not in custom_types[type_base]:
+                            print "ERROR: Custom types dict is invalid! (Map definition not found!)"
+                            sys.exit(1)
+                        if "format" not in custom_types[type_base]:
+                            print "ERROR: Custom types dict is invalid! (Map definition not found!)"
+                            sys.exit(1)
+                        for reg in custom_types[type_base]["regex"]:
+                            regex = re.compile(reg)
+                            match = regex.search(type)
+                            #print match.groups()
+                            if match:
+                                # Map check
+                                if len(match.groups()) == 0:
+                                    print "ERROR: No match groups defined for regex!"
+                                    sys.exit(1)
+                                if len(custom_types[type_base]["map"]) != len(match.groups()):
+                                    print "ERROR: Number of regex matches does not match number of maps! (Map has %i, match has %i)" % (len(custom_types[type_base]["map"]), len(match.groups()))
+                                    sys.exit(1)
+                                map_dict = {}
+                                map_res = match.groups()
+                                for i in xrange(0, len(map_res)):
+                                    # Validate
+                                    map_split = custom_types[type_base]["map"][i].split("=")
+                                    if len(map_split) != 2:
+                                        print "ERROR: Map specification is invalid! It should be variable=%TYPE%!"
+                                        sys.exit(1)
+                                    if not ((map_split[1][0] == "%") and (map_split[1][-1] == "%")):
+                                        print "ERROR: Map specification is invalid! The type part is invalid - it should be variable=%TYPE%!"
+                                        sys.exit(1)
+                                    map_name = map_split[0]
+                                    map_type = map_split[1].upper()
+                                    if map_type == "%INT%":
+                                        map_dict[map_name] = int(map_res[i])
+                                    elif map_type == "%FLOAT%":
+                                        map_dict[map_name] = float(map_res[i])
+                                    elif map_type == "%STRING%":
+                                        map_dict[map_name] = str(map_res[i])
+                                    else:
+                                        print "ERROR: Map specification is invalid! (Invalid type found - valid: %INT%, %FLOAT%, %STRING%)"
+                                        sys.exit(1)
+                        final_format = custom_types[type_base]["format"]
+                        # Variable replacement
+                        for var in map_dict.keys():
+                            final_format = final_format.replace("%%" + var + "%%", str(map_dict[var]))
+                        # Static variable replacement
+                        final_format = final_format.replace("%NAME%", sub_arg.name);
+                        final_format = final_format.replace("%POINTER%", "*");
+                        
+                        # Write it out!
+                        args_list.append(final_format)
+                        args_map[type] = final_format
                     else:
                         print "WARNING: Could not find equivalent C definition for type '%s'!" % type
                         print "Make sure you define it!"
@@ -276,17 +382,18 @@ for mod, name in modules:
                                 argument_dict[sub_arg.name] = []
                             if not attr in argument_dict[sub_arg.name]:
                                 argument_dict[sub_arg.name].append(attr)
-            header_fh.write("%s);\n" % ", ".join(args_list))
-            #print args_list
-            if len(subroutine.arguments) > 0:
-                final_arg_dict = {"input":[], "output":[]}
-                for arg in argument_dict:
-                    for attr in argument_dict[arg]:
-                        if attr == "intent(in)":
-                            final_arg_dict["input"].append(arg)
-                        if attr == "intent(out)":
-                            final_arg_dict["output"].append(arg)
-            ###############################################
-              
+                header_fh.write("%s);\n" % ", ".join(args_list))
+                #print args_list
+                if len(subroutine.arguments) > 0:
+                    final_arg_dict = {"input":[], "output":[]}
+                    for arg in argument_dict:
+                        for attr in argument_dict[arg]:
+                            if attr == "intent(in)":
+                                final_arg_dict["input"].append(arg)
+                            if attr == "intent(out)":
+                                final_arg_dict["output"].append(arg)
+                ###############################################
+            else:
+                header_fh.write(");\n")
 
 header_fh.close()
